1278f035369cf4ce09a7ccb7ce58cfa5fe9127bf
Luis Roseno <nando.roseno@gmail.com>
2021-09-07 18:04:51 -0300

    Favicon

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
