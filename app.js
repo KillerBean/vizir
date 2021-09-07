037f7b277c592a88b00b000a5f9fe75ef10a0bed
Luis Roseno <nando.roseno@gmail.com>
2021-09-07 18:37:50 -0300

    Removed deprecated body-parser and added rate limiting package

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 
@@ -25,8 +24,8 @@ app.set('port', process.env.PORT || 3000);
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(limiter);
